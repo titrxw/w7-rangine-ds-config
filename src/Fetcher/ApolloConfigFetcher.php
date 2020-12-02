cec8b2c3a196a1d17e3da06648c25c1b6c17ec90
titrxw <1120309488@qq.com>
2020-12-02 23:23:59 +0800

    init

--- src/Fetcher/ApolloConfigFetcher.php
+++ src/Fetcher/ApolloConfigFetcher.php
@@ -2,8 +2,31 @@
 
 namespace W7\Config\Fetcher;
 
-class ApolloConfigFetcher extends ConfigFetcherAbstract {
-	public function fetch() {
+use Org\Multilinguals\Apollo\Client\ApolloClient;
 
+class ApolloConfigFetcher extends ConfigFetcherAbstract {
+    protected $config;
+
+    public function __construct(array $config) {
+        $this->config = $config;
+    }
+
+    public function fetch() {
+        $apollo = new ApolloClient($this->config['server'], $this->config['app_id'], (array)$this->config['namespace'] ?? []);
+
+        //如果需要灰度发布，指定clientIp
+        /*
+         * $clientIp = '10.160.2.131';
+         * if (isset($clientIp) && filter_var($clientIp, FILTER_VALIDATE_IP)) {
+         *    $apollo->setClientIp($clientIp);
+         * }
+         */
+
+        //从apollo上拉取的配置默认保存在脚本目录，可自行设置保存目录
+        $apollo->save_dir = RUNTIME_PATH . '/config';
+
+        $error = $apollo->start(function () {
+
+        });
 	}
 }
